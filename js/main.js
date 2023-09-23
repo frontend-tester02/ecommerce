ec3211dee8f46f46db566c8ad42b4bd20beee022
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2023-09-23 23:37:05 +0900

    Update active showcas img on thumbnail click

--- js/main.js
+++ js/main.js
@@ -1,13 +1,45 @@
+const modifiers = {
+  imgThumbnailActive: 'img-showcase__thumbnail--active'
+}
+
+
+// Shopping cart modal
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
+
+
 
 
 if (elSiteHeaderCartLink) {
   elSiteHeaderCartLink.addEventListener('click', function (evt) {
     evt.preventDefault();
 
-    elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open')
+    elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
 
-  })
+  });
 
 }
+
+// Image showcase
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img');
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.js-img-showcase-thumbnail-button');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+
+
+elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+  elButton.addEventListener('click', function () {
+    // Remove active state from all buttons
+    elsImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+    });
+
+    // Add active state to clicked button
+    elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+    // Update active img src  accordingly
+    elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+    elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetina} 2x `;
+
+  });
+
+});
